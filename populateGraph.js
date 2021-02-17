0e7c5bb3aa50b2137ec47027c767b0287d15360a
Dinakaran Sankaranarayanan <dinakaranonline@gmail.com>
2021-02-17 18:44:44 +0530

    Update populateGraph.js

--- populateGraph.js
+++ populateGraph.js
@@ -9,15 +9,17 @@ function Graph() {
       neighbors[u] = [];
     }
     neighbors[u].push(v);
-    if (neighbors[v] === undefined) {  // Also add the edge v -> u in order
-      neighbors[v] = [];               // to implement an undirected graph.
-    }                                  // For a directed graph, delete
-    neighbors[v].push(u);              // these four lines.
+    //Uncomment this section for undirected graph 
+   // if (neighbors[v] === undefined) {  // Also add the edge v -> u in order
+     // neighbors[v] = [];               // to implement an undirected graph.
+    //}                                  // For a directed graph, delete
+    //neighbors[v].push(u);              // these four lines.
   };
 
   return this;
 }
 
+//Breadth First Retrieval implementation
 function bfs(graph, source) {
   var queue = [ { vertex: source, count: 0 } ],
       visited = { source: true },
@@ -35,6 +37,7 @@ function bfs(graph, source) {
   }
 }
 
+//Determine the shortest path between two given location
 function shortestPath(graph, source, target) {
   var distance=0;
   if (source == target) {   // Delete these four lines if
@@ -46,13 +49,14 @@ function shortestPath(graph, source, target) {
       visited = { source: true },
       predecessor = {},
       tail = 0;
+  
+       
   while (tail < queue.length) {
     var u = queue[tail++],  // Pop a vertex off the queue.
         neighbors = graph.neighbors[u];
     for (var i = 0; i < neighbors.length; ++i) {
       var v = neighbors[i];
       if (visited[v]) {
-         
         continue;
       }
       visited[v] = true;
@@ -60,13 +64,12 @@ function shortestPath(graph, source, target) {
       if (v === target) {   // Check if the path is complete.
         var path = [ v ];   // If so, backtrack through the path.
         while (u !== source) {
-            
           path.push(u);
-          distance++    
+          distance++;    
           u = predecessor[u];
         }
         path.push(u);
-        distance++
+        distance++;
         path.reverse();
         print(path.join(' > '));
         console.log("distance###"+distance);
@@ -79,86 +82,79 @@ function shortestPath(graph, source, target) {
   print('there is no path from ' + source + ' to ' + target);
 }
 
+
+//Print path for debugging
 function print(s) {  // A quick and dirty way to display output.
   s = s || '';
   console.log(s);
 }
 
 
-
-
+//Main Lambda function entry point
 exports.handler = async (event) => {
-    //console.log("event header"+event.header)
-   // console.log("eneted here");
-    //console.log("event body"+event.body["graph"]);
     var parsedBody = JSON.parse(event.body);
     console.log("parsedBody###"+parsedBody.graph);
+    //parse json body and build the graph 
     var chars = parsedBody.graph.split(',');
-    
     var graph = new Graph();
+    var locList = [];
+    var filteredLocList = [];
     var i;
+    //build the graph
     for (i = 0; i < chars.length; i++) { 
             console.log("array "+i+" is =" +chars[i]);
             var nodeSplit = chars[i].split('->');
             var source = nodeSplit[0];
             var destination = nodeSplit[1];
             graph.addEdge(source, destination);
+            locList.push(source);
+            locList.push(destination);
      }
      
-
- 
-  //graph.addEdge('Logan', 'SaltLakeCity');
-  //graph.addEdge('Logan', 'CiderCity');
-  //graph.addEdge('SaltLakeCity', 'Provo');
-
-
+    //remove duplicates from cities   
+    filteredLocList = locList.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+    
+      //Iterate through each source in the loop and within that loop , iterate through destionation loop
+      //If it is a valid route, compute distance and add it to the DynamoDB table 
+      for(let i = 0; i < filteredLocList.length; i++){
+          console.log("filteredlist###"+filteredLocList[i]);
+          for(let j = 0; j < filteredLocList.length; j++){
+              console.log("source####"+filteredLocList[i]);
+              console.log("destination####"+filteredLocList[j]);
+              //if source and destination are different alone , calculate the shortest distance 
+               if(filteredLocList[i]!=filteredLocList[j]){
+                 try{
+                    var distance = shortestPath(graph, filteredLocList[i], filteredLocList[j]);
+                    console.log("valid path, adding to dynamodb distance###"+distance);
+                     var params = {
+                        TableName : "distance_graph",
+                        Item: { 
+                              sourceCity :filteredLocList[i], 
+                              targetCity: filteredLocList[j], 
+                              distance: distance
+                            }
+                     };
+        
+                  const result = await docClient.put(params).promise();
+                  console.log("record added for "+[i]+result);
+                  
+                 }
+                  catch(error){
+                    console.log("valid path does not exist, so not adding them");
+                }
+              }
+          }
+      }
+    
   //bfs(graph, 'Logan');
-  print();
-  var distanceret = shortestPath(graph, 'SaltLakeCity', 'Provo');
-  console.log("shortest path returned ###"+distanceret);
-  print();
-  shortestPath(graph, 'Logan', 'Provo');
-  print();
-  shortestPath(graph, 'Provo', 'CiderCity');
-  print();
-  shortestPath(graph, 'SaltLakeCity', 'Logan');
-  print();
+  //print();
+  //var distanceret = shortestPath(graph, 'SaltLakeCity', 'Provo');
+  //console.log("shortest path returned ###"+distanceret);
+  //print();
   
-    
-    var distanceArray= [ ]; 
-    var record1 = {sourceCity:"sourceCity1", targetCity:"targetCity1", distance:"1000"};
-    var record2 = {sourceCity:"sourceCity2", targetCity:"targetCity2", distance:"2000"};
-    var record3 = {sourceCity:"sourceCity3", targetCity:"targetCity3", distance:"3000"};
-    var record4 = {sourceCity:"sourceCity4", targetCity:"targetCity4", distance:"4000"};
-    
-    distanceArray.push(record1); 
-    distanceArray.push(record2); 
-    distanceArray.push(record3); 
-    distanceArray.push(record4);
-    
-    for(let i = 0; i < distanceArray.length; i++){
-        
-        //console.log("source city####"+distanceArray[i].sourceCity);
-        //console.log("target city####"+distanceArray[i].targetCity);
-        //console.log("distance####"+distanceArray[i].distance);
-        
-        var params = {
-            TableName : "distance_graph",
-            Item: { 
-                sourceCity : distanceArray[i].sourceCity, 
-                targetCity: distanceArray[i].targetCity, 
-                distance: distanceArray[i].distance 
-             
-            }
-        };
-        
-         //const result = await docClient.put(params).promise();
-         //console.log("record added for "[i]);
-        
-    }
-    
-  
-    // TODO implement
+
+
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
